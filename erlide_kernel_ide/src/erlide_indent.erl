8509a144f6539fbe549ebcd2e2bde27b7002fb57
Vlad Dumitrescu <vladdu55@gmail.com>
2016-03-14 15:57:47 +0100

    indent fixes

--- erlide_kernel_ide/src/erlide_indent.erl
+++ erlide_kernel_ide/src/erlide_indent.erl
@@ -40,7 +40,8 @@ default_indent_prefs() ->
      {fun_body, 5},
      {paren, 1},
      {'<<', 2},
-     {end_paren, 0}].
+     {end_paren, 0},
+     {record_fields, 2}].
 
 %%
 %% API Functions
@@ -328,6 +329,7 @@ i_expr_list(R, I) ->
     i_expr_list(R, I, none).
 
 i_expr_list(R0, I0, A0) ->
+    ?D(apa),
     R1 = i_comments(R0, I0),
     ?D(R1),
     {R2, A1} = i_expr(R1, I0, A0),
@@ -679,7 +681,13 @@ i_record([#token{kind='#'} | R0], I0) ->
             ?D(R4),
             {R4, I#i.anchor};
         '{' ->
-            i_expr(R2, I, I#i.anchor);
+            %i_expr(R2, I, I#i.anchor);
+            R3 = i_kind('{', R2, I),       % }
+            A = R1,
+            I1 = i_with(record_fields, A, I),
+            R4 = i_end_paren_or_expr_list(R3, I1#i{in_block=false}),
+            I2 = i_with(end_paren, A, I),
+            {i_end_paren(R4, I2), A};
         '[' ->
             i_expr(R2, I, none);
         '?' ->
@@ -775,6 +783,9 @@ i_declaration(R0, I) ->
         [#token{kind=atom, value='type'} | _] ->
             R2 = i_kind(atom, R1, I),
             i_type(R2, I);
+        [#token{kind=atom, value=record} | _] ->
+            R2 = i_kind(atom, R1, I),
+            i_record_def(R2, I);
         _ ->
             {R2, _A} = i_expr(R1, I, none),
             i_kind(dot, R2, I)
@@ -827,6 +838,21 @@ i_spec(R0, I) ->
                 i_spec_list(R0, I)
         end,
     i_dot_or_semi(R, I).
+
+i_record_def(R0, I0) ->
+    R1 = i_kind('(', R0, I0),
+    Kind = i_sniff(R1),
+    R2 = i_kind(Kind, R1, I0),
+    R3 = i_kind(',', R2, I0),
+    R4 = i_kind('{', R3, I0),
+    A = R1,
+    I1 = i_with(record_fields, A, I0),
+    ?D(I1#i.current),
+    R5 = i_expr_list(R4, I1, R4),
+    ?D(R5),
+    R6 = i_kind('}', R5, I0),
+    R7 = i_kind(')', R6, I0),
+    i_kind(dot, R7, I0).
 
 i_fun_clause(R0, I0) ->
     R1 = i_comments(R0, I0),
